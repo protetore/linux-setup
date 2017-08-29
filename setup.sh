a3b5c819838e8705e054cd65b86bf4117c0cd82c
Diego <diego@commodityvectors.com>
2017-08-29 16:17:46 +0100

    Add diff-so-fancy as git diff

--- setup.sh
+++ setup.sh
@@ -389,6 +389,21 @@ fi
 # diff-so-fancy (https://github.com/so-fancy/diff-so-fancy)
 dsub "Installing diff-so-fancy..."
 execute curl -skL -o diff-so-fancy https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy
+chmod +x ./diff-so-fancy
+sudo mv ./diff-so-fancy /usr/local/bin/
+# Config git diff colors
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+git config --global color.diff-highlight.oldNormal "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+git config --global color.diff.meta "227"
+git config --global color.diff.frag "magenta bold"
+git config --global color.diff.commit "227 bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+git config --global color.diff.whitespace "red reverse"
 
 # Idea
 # Plugins: Material UI Theme, Darcula Syntax Theme, Main menu Toggle
